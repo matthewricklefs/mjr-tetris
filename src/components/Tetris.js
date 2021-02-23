8ea2226b821b605e0155c37cd39362016b14bf61
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-23 16:16:21 -0500

    introduces styled-components by adding styles directory with unique styles for each component being used in the game

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,21 +6,24 @@ import { createStage } from "../gameHelpers";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()}  />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
 
-        <StartButton />
-      </aside>
-    </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
