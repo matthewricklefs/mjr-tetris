7d62dd9a67a7ca43833f1e93f46925744d7ee682
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-02-23 20:39:17 -0500

    updates Tetris.js to reflect the resetPlayer property being passed properly so that the Tetrominos do not throw an error when it reaches the bottom of the game level

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -6,12 +6,12 @@ export const useStage = (player, resetPlayer) => {
 
   useEffect(() => {
     const updateStage = (prevStage) => {
-      //First flush the stage (clear from previous render)
+      // First flush the stage
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
       );
 
-      //then draw the tetromino
+      // Then draw the tetromino
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -22,12 +22,16 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
       return newStage;
     };
 
     setStage((prev) => updateStage(prev));
-  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
+  }, [player, resetPlayer]);
 
   return [stage, setStage];
 };
